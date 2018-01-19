0b3464bd5945005201453557c1c9f87648b1c41d
cbtak <cbtak@hotmail.com>
2018-01-19 13:57:20 +0800

    dialog 拖拽功能实现及错位样式处理

--- simpleHeadBodyTemplet/edit/drag.js
+++ simpleHeadBodyTemplet/edit/drag.js
@@ -1,62 +1,50 @@
-var Drag = {
-    createDrag : function (targetClass) {
-        var draggingObj=null; //dragging Dialog
-        var diffX=0;
-        var diffY=0;
-
-        function validateHandler(e){
-            var target=e.target;
-            if(!target || !target.className || target.className.indexOf(targetClass)==-1) {
-                return null;
-            } else {
-                return target.offsetParent;
-            }
-            // while(target && target.className && target.className.indexOf(targetClass)==-1){
-            //     target=target.offsetParent;
-            // }
-            // if(target!=null){
-            //     return target.offsetParent;
-            // }else{
-            //     return null;
-            // }
+window.Drag = function(targetClass) {
+	let dragTarget = null;
+	
+	function validateHandler(e){
+		var target = e.target;
+        if(target && target.className && target.className.indexOf(targetClass) != -1) {
+            return target.offsetParent;
+        } else {
+            return null;
         }
+	}
 
-        function mouseHandler(e){
-            switch(e.type) {
-                case 'mousedown':
-                    draggingObj=validateHandler(e);//验证是否为可点击移动区域
-                    if(draggingObj!=null){
-                        diffX=e.clientX-draggingObj.offsetLeft;
-                        diffY=e.clientY-draggingObj.offsetTop;
-                    }
-                    break;
+	function mouseHandler(e){
+	    switch(e.type) {
+	        case 'mousedown':
+	            dragTarget = validateHandler(e);//验证是否为可点击移动区域
+	            if(dragTarget){
+	                diffX = e.clientX - dragTarget.offsetLeft;
+	                diffY = e.clientY - dragTarget.offsetTop;
+	            }
+	            break;
 
-                case 'mousemove':
-                    if(draggingObj){
-                        draggingObj.style.left=(e.clientX-diffX)+'px';
-                        draggingObj.style.top=(e.clientY-diffY)+'px';
-                    }
-                    break;
+	        case 'mousemove':
+	            if(dragTarget) {
+	                dragTarget.style.left = (e.clientX - diffX) + 'px';
+	                dragTarget.style.top = (e.clientY - diffY) + 'px';
+	            }
+	            break;
 
-                case 'mouseup':
-                    draggingObj =null;
-                    diffX=0;
-                    diffY=0;
-                    break;
-            }
-        };
+	        case 'mouseup':
+	            dragTarget = null;
+	            diffX = 0;
+	            diffY = 0;
+	            break;
+	    }
+	};
 
-        return {
-            enable:function(){
-                document.addEventListener('mousedown',mouseHandler);
-                document.addEventListener('mousemove',mouseHandler);
-                document.addEventListener('mouseup',mouseHandler);
-            },
-            disable:function(){
-                document.removeEventListener('mousedown',mouseHandler);
-                document.removeEventListener('mousemove',mouseHandler);
-                document.removeEventListener('mouseup',mouseHandler);
-            }
-        }
-    }
-}
+	return {
+	    enable:function(){
+	        document.addEventListener('mousedown', mouseHandler);
+	        document.addEventListener('mousemove', mouseHandler);
+	        document.addEventListener('mouseup', mouseHandler);
+	    },
+	    disable:function(){
+	        document.removeEventListener('mousedown', mouseHandler);
+	        document.removeEventListener('mousemove', mouseHandler);
+	        document.removeEventListener('mouseup', mouseHandler);
+	    }
+	}
+};

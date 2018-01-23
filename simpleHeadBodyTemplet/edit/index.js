f39db319f65bc2ee84d8b0f55f9d46f4cde0fdf0
cbtak <cbtak@hotmail.com>
2018-01-23 13:39:14 +0800

    表格编辑拖拽时控件大小变化处理

--- simpleHeadBodyTemplet/edit/index.js
+++ simpleHeadBodyTemplet/edit/index.js
@@ -24,7 +24,6 @@
                             label: "名称",
                             prefix_icon: "el-icon-search",
                             type: "text",
-                            item_type: 1,
                             placeholder: "名称", disabled: true
                         },
                         {
@@ -285,97 +284,92 @@
                         {
                             key: "text",
                             data_key: "text",
-                            label: "text",
+                            label: "文本",
                             type: "text",
-                            item_type: 2,
                             sortable: true,
-                            placeholder: "text"
+                            width : 120,
+                            placeholder: "文本"
                         },
                         {
                             key: "number",
                             data_key: "number",
-                            label: "number",
+                            label: "数值",
                             type: "number",
-                            item_type: 2,
                             sortable: true,
-                            placeholder: "number"
+                            width : 100,
+                            placeholder: "数值"
                         },
                         {
                             key: "scaler",
                             data_key: "scaler",
-                            label: "scaler",
+                            label: "计数器",
                             type: "scaler",
-                            item_type: 2,
                             sortable: true,
                             width : 100,
-                            placeholder: "scaler"
+                            placeholder: "计数器"
                         },
                         {
                             key: "textarea",
                             data_key: "textarea",
-                            label: "textarea",
+                            label: "大文本",
                             type: "textarea",
-                            item_type: 2,
-                            placeholder: "textarea"
+                            width : 160,
+                            placeholder: "大文本"
                         },
                         {
-                            key: "radio_item",
-                            data_key: "radio_item",
+                            key: "radio",
+                            data_key: "radio",
                             label: "单选框",
                             type: "radio",
-                            item_type: 1,
-                            disabled : true,
-                            options: [{id: "01", name: "无早", hideitem: false}, {
-                                id: "02",
-                                name: "单早",
-                                desabled: true
-                            }, {id: "03", name: "双早"}],
+                            width : 180,
+                            options: [
+                                {id: "01", name: "无早", hideitem: false},
+                                {id: "02", name: "单早", desabled: true},
+                                {id: "03", name: "双早"}],
                             option_key: "id",
                             option_label: "name",
                             option_disabled_key: "desabled",
                             option_hide_key: "hideitem"
                         },
                         {
-                            key: "checkbox_item",
-                            data_key: "checkbox_item",
+                            key: "checkbox",
+                            data_key: "checkbox",
                             label: "复选框",
                             type: "checkbox",
-                            item_type: 1,
-                            options: [{id: '1', name: '上海'}, {id: '2', name: '北京', desabled: true}, {
-                                id: '3',
-                                name: '广州',
-                                hide: true
-                            }, {id: '4', name: '深圳'}],
+                            width : 180,
+                            options: [
+                                {id: '1', name: '上海'},
+                                {id: '2', name: '北京', desabled: true},
+                                {id: '3', name: '广州', hide: true},
+                                {id: '4', name: '深圳'}
+                            ],
                             option_key: "id",
                             option_label: "name",
                             option_disabled_key: "desabled",
                             option_hide_key: "hide"
                         },
                         {
-                            key: "selectitem",
-                            data_key: "selectitem",
+                            key: "select",
+                            data_key: "select",
                             label: "下拉选择",
                             type: "select",
-                            item_type: 1,
+                            width : 120,
                             placeholder: "请选择",
                             multiple: false,
                             filterable: true,
                             collapse_tags: false,
                             separator: ",",
-                            options: [{value: 1, name: "项1"}, {value: 2, name: "项2"}, {value: 3, name: "项3"}, {
-                                value: 4,
-                                name: "项目4"
-                            }],
+                            options: [{value: 1, name: "项1"}, {value: 2, name: "项2"}, {value: 3, name: "项3"}, {value: 4,name: "项目4"}],
                             option_key: "value",
                             option_label: "name",
-                            option_disabled_key: "desabled",
+                            option_disabled_key: "desabled"
                         },
                         {
-                            key: "cascaderitem",
-                            data_key: "cascaderitem",
+                            key: "cascader",
+                            data_key: "cascader",
                             label: "级联选择",
                             type: "cascader",
-                            item_type: 1,
+                            width : 120,
                             placeholder: "请选择",
                             show_separator: "\\",
                             filterable: true,
@@ -400,37 +394,35 @@
                             option_disabled_key: "desabled"
                         },
                         {
-                            key: "switchitem",
-                            data_key: "switchitem",
-                            label: "是否活动",
+                            key: "switch",
+                            data_key: "switch",
+                            label: "状态",
                             type: "switch",
-                            item_type: 1,
-                            active_value: "on",
-                            inactive_value: "off",
+                            width : 60,
+                            active_value: true,
+                            inactive_value: false
                         },
                         {
-                            key: "slideritem",
-                            data_key: "slideritem",
+                            key: "slider",
+                            data_key: "slider",
                             label: "滑块控件",
                             type: "slider",
-                            item_type: 1
-
+                            width : 140
                         },
                         {
-                            key: "rate_item",
-                            data_key: "rate_item",
+                            key: "rate",
+                            data_key: "rate",
                             label: "评分",
                             type: "rate",
                             show_text: true,
-                            item_type: 1
-
+                            width : 160
                         },
                         {
                             key: "time_select",
                             data_key: "time_select",
                             label: "时间下拉",
                             type: "time-select",
-                            item_type: 1
+                            width : 120
                         },
                         {
                             key: "time_picker",
@@ -442,14 +434,14 @@
                                 selectableRange: ['10:30:00 - 12:00:00', '14:30:00 - 15:30:00'], // 暂无效果，待详细测试
                                 format: "HH:mm"
                             },
-                            item_type: 1
+                            width : 120
                         },
                         {
                             key: "date_picker",
                             data_key: "date_picker",
                             label: "日期控件",
                             type: "date-picker",
-                            item_type: 1
+                            width : 120
                         },
                         {
                             key: "date_picker_year",
@@ -457,7 +449,7 @@
                             label: "年度",
                             type: "date-picker",
                             date_type: "year",
-                            item_type: 1
+                            width : 100
                         },
                         {
                             key: "date_picker_month",
@@ -466,9 +458,9 @@
                             type: "date-picker",
                             date_type: "month",
                             title: function (appData) {
-                                return isNotNull(appData.value) ? Number(appData.value) + "月" : appData.value;
+                                return isNotNull(appData.value) ? Number(appData.value) + "M" : appData.value;
                             },
-                            item_type: 1
+                            width : 100
                         },
                         {
                             key: "date_picker_week",
@@ -479,7 +471,7 @@
                             title: function (appData) {
                                 return isNotNull(appData.value) ? moment(appData.value).format("YYYY年第W周") : appData.value;
                             },
-                            item_type: 1
+                            width : 140
                         },
                         {
                             key: "date_picker_datetime",
@@ -487,7 +479,7 @@
                             label: "完整时间",
                             type: "date-picker",
                             date_type: "datetime",
-                            item_type: 1
+                            width : 140
                         },
                         {
                             key: "date_picker_datetimerange",
@@ -496,7 +488,7 @@
                             type: "date-picker",
                             range_separator: "至", start_placeholder: "开始日期", end_placeholder: "结束日期",
                             date_type: "datetimerange",
-                            item_type: 1
+                            width : 200
                         },
                         {
                             key: "date_picker_daterange",
@@ -505,21 +497,21 @@
                             type: "date-picker",
                             range_separator: "至", start_placeholder: "开始日期", end_placeholder: "结束日期",
                             date_type: "daterange",
-                            item_type: 1
+                            width : 160
                         },
                         {
                             key: "color_item",
                             data_key: "color_item",
                             label: "H5颜色控件",
                             type: "color",
-                            item_type: 1
+                            width : 80
                         },
                         {
                             key: "color_item2",
                             data_key: "color_item2",
                             label: "Ele颜色控件",
                             type: "color-picker",
-                            item_type: 1
+                            width : 80
                         },
                         {
                             key: "ref_item",
@@ -530,7 +522,7 @@
                                 type: "table"
                             },
                             // disabled:true,
-                            item_type: 2,
+                            width : 120,
                             readonly: true,
                             placeholder: "参照"
                         }
